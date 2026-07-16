0cf67c7c0a64a190708a9d604a1bc5d051ab7a8c
Pilartxo <pilarruete@gmail.com>
2026-07-16 12:49:21 +0200

    Update cargarComponentes.js

--- js/cargarComponentes.js
+++ js/cargarComponentes.js
@@ -1,1 +1,17 @@
 
+document.addEventListener("DOMContentLoaded", function () {
+
+    cargar("menu", "componentes/menu.html");
+    cargar("footer", "componentes/footer.html");
+
+});
+
+function cargar(id, archivo){
+
+    fetch(archivo)
+        .then(respuesta => respuesta.text())
+        .then(html => {
+            document.getElementById(id).innerHTML = html;
+        });
+
+}
